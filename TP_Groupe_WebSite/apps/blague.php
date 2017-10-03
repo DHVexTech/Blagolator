a9c29a545f7a89d88f2141a29970cbc75d53d6bc
Unknown <Alex110397@gmail.com>
2017-10-03 10:15:43 +0200

    Added second party of website

--- TP_Groupe_WebSite/apps/blague.php
+++ TP_Groupe_WebSite/apps/blague.php
@@ -1,25 +1,27 @@
 <?php
-$home = "titi";
 
+$category = $_POST["Category"];
+
+$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
 /* Afficher une variable et arrête le script pour des raisons de débogage */
 function dd($data) {
- echo "<pre>";
- die(var_dump($data));
- echo "</pre>";
-}
-
-// Génère : <body text='black'>
-$bodytag = str_replace("%body%", "black", "<body text='%body%'>");
-
+    echo "<pre>";
+    die(var_dump($data));
+    echo "</pre><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+   }
 
 /* Adresse de l'URL sur laquelle on va taper pour récuperer le contenu JSON */
-$api_url = "http://10.8.110.213:8080/joke";
+$api_url = "http://10.8.110.213:8080/joke/".$category;
 /* Le contenu brut (au format JSON) issu de l'API */
 $api_content = file_get_contents($api_url);
 /* Transformation du JSON en Tableau PHP */
 $api_content_array = json_decode($api_content);
-/* On affiche ce que l'on a réussi à obtenir et on s'arrête ici */
-//dd($api_content_array);
 
-require('views/home.phtml');
+/* On affiche ce que l'on a réussi à obtenir et on s'arrête ici */
+
+$tab = $api_content_array->content;
+//dd($tab[0]);
+
+
+require('views/blague.phtml');
 ?>
--- TP_Groupe_WebSite/index.php
+++ TP_Groupe_WebSite/index.php
@@ -1,6 +1,7 @@
 <?php
 require('./config.php');
 $page = 'home';
+if(isset($_GET["page"])) $page = $_GET["page"];
 require('apps/skel.php');
 
 ?>
